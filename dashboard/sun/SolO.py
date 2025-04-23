9ab10a33616b8b3e70ec5350cb14a6eeff048949
mgjeon <mgjeon@khu.ac.kr>
2025-04-23 10:13:28 +0900

    init commit

---
+++ dashboard/sun/SolO.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown(
+    """
+    # Solar Orbiter
+
+    ![SolO](https://www.nasa.gov/wp-content/uploads/2023/03/solar_orbiter_artist_impression_20190916_1.jpg)
+
+    Launch: [February 2020](https://www.esa.int/Science_Exploration/Space_Science/Solar_Orbiter)
+    """
+)
+
+st.markdown(
+    """
+    - [MPS/EUI](https://www.mps.mpg.de/solar-physics/solar-orbiter-eui)
+    - [SIDC/EUI](https://www.sidc.be/EUI)
+    """
+)
